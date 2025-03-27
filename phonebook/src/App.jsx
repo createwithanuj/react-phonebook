590ade57767c571f062471a0a07be41da7dd864f
Anuj Kumar Gautam <61941950+createwithanuj@users.noreply.github.com>
2025-03-27 17:11:48 +0000

    Fetch initial data from server using axios and useEffect

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import UserForm from './UserForm';
 import SearchPerson from './SearchPerson';
 import DisplayContacts from './DisplayContacts';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([])
   
+  useEffect(()=>{
+    axios
+    .get('https://bug-free-giggle-95rp7v4vw963xj6x-3001.app.github.dev/persons')
+    .then(response => {
+      setPersons(response.data)
+    });
+  }, []);
+
+
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchPattern, setSearchPattern] = useState('');
@@ -39,6 +44,7 @@ const App = () => {
     setNewNumber('');
   };
 
+  
   const personsToShow = searchPattern
     ? persons.filter(person =>
         person.name.toLowerCase().includes(searchPattern.toLowerCase())
